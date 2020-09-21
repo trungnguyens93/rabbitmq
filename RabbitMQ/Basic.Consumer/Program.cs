140d6d40fefc6a0828f53991693ce2039fba0c01
trungnguyen <trungnguyens93@outlook.com>
2020-09-21 17:53:05 +0700

    [RabbitMQ] Basic

---
+++ RabbitMQ/Basic.Consumer/Program.cs
@@ -0,0 +1,53 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+namespace Basic.Consumer
+{
+    class Program
+    {
+        private const string _HostName = "localhost";
+        private const string _Username = "guest";
+        private const string _Password = "guest";
+
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory()
+            {
+                HostName = _HostName,
+                UserName = _Username,
+                Password = _Password
+            };
+
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(
+                    queue: "basic-queue",
+                    durable: false,
+                    exclusive: false,
+                    autoDelete: false,
+                    arguments: null
+                );
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) => {
+                    var body = ea.Body.ToArray();
+                    var message = Encoding.UTF8.GetString(body);
+
+                    Console.WriteLine(" [X] Received: {0}", message);
+                };
+
+                channel.BasicConsume(
+                    queue: "basic-queue",
+                    autoAck: true,
+                    consumer: consumer
+                );
+            }
+
+            Console.WriteLine(" Press [Enter] to exit");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ RabbitMQ/Basic.Producer/Program.cs
@@ -0,0 +1,49 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace Basic.Producer
+{
+    class Program
+    {
+        private const string _HostName = "localhost";
+        private const string _Username = "guest";
+        private const string _Password = "guest";
+
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory()
+            {
+                HostName = _HostName,
+                UserName = _Username,
+                Password = _Password
+            };
+
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(
+                    queue: "basic-queue",
+                    durable: false,
+                    exclusive: false,
+                    autoDelete: false,
+                    arguments: null
+                );
+
+                string message = "Hello World!";
+                var body = Encoding.UTF8.GetBytes(message);
+                channel.BasicPublish(
+                    exchange: "",
+                    routingKey: "basic-queue",
+                    basicProperties: null,
+                    body: body
+                );
+
+                Console.WriteLine(" [X] Sent: {0}", message);
+            }
+
+            Console.WriteLine(" Press [Enter] to exit!!!");
+            Console.ReadLine();
+        }
+    }
+}
