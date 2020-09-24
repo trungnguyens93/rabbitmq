9703728bcba084d5d4ce8ec52d6525e77b3cf850
trungnguyen <trungnguyens93@outlook.com>
2020-09-24 14:12:13 +0700

    [New] Publish/Subscribe

---
+++ RabbitMQ/Publish.Consumer/Program.cs
@@ -0,0 +1,57 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+namespace Publish.Consumer
+{
+    class Program
+    {
+        private const string _Hostname = "localhost";
+        private const string _Username = "guest";
+        private const string _Password = "guest";
+
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory()
+            {
+                HostName = _Hostname,
+                UserName = _Username,
+                Password = _Password
+            };
+
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.ExchangeDeclare(
+                    exchange: "publish-exchange",
+                    type: ExchangeType.Fanout
+                );
+
+                var queueName = channel.QueueDeclare().QueueName;
+                channel.QueueBind(
+                    queue: queueName, 
+                    exchange: "publish-exchange", 
+                    routingKey: "", 
+                    arguments: null
+                );
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) => {
+                    var body = ea.Body.ToArray();
+                    var message = Encoding.UTF8.GetString(body);
+
+                    Console.WriteLine(" [x] Received: {0}", message);
+                };
+
+                channel.BasicConsume(
+                    queue: queueName,
+                    autoAck: true,
+                    consumer: consumer
+                );
+
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ RabbitMQ/Publish.Producer/Program.cs
@@ -0,0 +1,48 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace Publish.Producer
+{
+    class Program
+    {
+        private const string _Hostname = "localhost";
+        private const string _Username = "guest";
+        private const string _Password = "guest";
+
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() 
+            { 
+                HostName = _Hostname,
+                UserName = _Username,
+                Password = _Password
+            };
+
+            using (var connection = factory.CreateConnection()) 
+            using (var channel = connection.CreateModel())
+            {
+                channel.ExchangeDeclare(
+                    exchange: "publish-exchange",
+                    type: ExchangeType.Fanout
+                );
+
+                string message = GetMessage(args);
+                var body = Encoding.UTF8.GetBytes(message);
+                channel.BasicPublish(
+                    exchange: "publish-exchange",
+                    routingKey: "",
+                    basicProperties: null,
+                    body: body
+                );
+
+                Console.WriteLine(" [x] Sent: {0}", message);
+            }
+        }
+
+        private static string GetMessage(string[] args)
+        {
+            return args.Length > 0 ? string.Join(' ', args) : "Hello World!!!";
+        }
+    }
+}
