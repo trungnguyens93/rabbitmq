56a6fbf7c8283b5370dcee566345543a09223ff8
trungnguyens93 <trungnguyens93@gmail.com>
2020-09-25 18:01:35 +0700

    [New] Routing

---
+++ RabbitMQ/Routing.Consumer/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace Routing.Consumer
+{
+    class Program
+    {
+        private static string _Hostname = "localhost";
+        private static string _Username = "guest";
+        private static string _Password = "guest";
+
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory()
+            {
+                HostName = _Hostname,
+                UserName = _Username,
+                Password = _Password
+            };
+
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.ExchangeDeclare(
+                    exchange: "routing-exchange",
+                    type: ExchangeType.Direct
+                );
+
+                if (args.Length < 1)
+                {
+                    Console.Error.WriteLine("Usage: {0} [info] [warning] [error]",
+                                            Environment.GetCommandLineArgs()[0]);
+                    Console.WriteLine(" Press [enter] to exit.");
+                    Console.ReadLine();
+                    Environment.ExitCode = 1;
+                    return;
+                }
+
+                var queueName = channel.QueueDeclare().QueueName;
+
+                foreach (var severity in args)
+                {
+                    channel.QueueBind(
+                        queue: queueName,
+                        exchange: "routing-exchange",
+                        routingKey: severity,
+                        arguments: null
+                    );
+                }
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) => {
+                    var body = ea.Body.ToArray();
+                    var message = Encoding.UTF8.GetString(body);
+                    var routingKey = ea.RoutingKey;
+
+                    Console.WriteLine(" [x] Received: {0}: {1}", routingKey, message);
+                };
+
+                channel.BasicConsume(
+                    queue: queueName,
+                    autoAck: true,
+                    consumer: consumer
+                );
+
+
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ RabbitMQ/Routing.Producer/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using RabbitMQ.Client;
+
+namespace Routing.Producer
+{
+    class Program
+    {
+        private const string _Hostname = "localhost";
+        private const string _Username = "guest";
+        private const string _Password = "guest";
+
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory()
+            {
+                HostName = _Hostname,
+                UserName = _Username,
+                Password = _Password
+            };
+
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.ExchangeDeclare(
+                    exchange: "routing-exchange",
+                    type: ExchangeType.Direct
+                );
+
+                var severity = (args.Length > 0) ? args[0] : "info";
+
+                var message = (args.Length > 1)
+                                ? string.Join(' ', args.Skip(1).ToArray())
+                                : "Hello world!!!";
+
+                var body = Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish(
+                    exchange: "routing-exchange",
+                    routingKey: severity,
+                    basicProperties: null,
+                    body: body
+                );
+            }
+        }
+    }
+}
